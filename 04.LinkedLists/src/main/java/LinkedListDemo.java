dd700827eebc37c2ba93ed56b88d8ec53de02569
Fatih-Cyber <fatih@cybertekschool.com>
2022-09-09 18:39:24 +0300

    LinkedLists Module added

--- 04.LinkedLists/src/main/java/LinkedListDemo.java
+++ 04.LinkedLists/src/main/java/LinkedListDemo.java
@@ -1,2 +1,16 @@
 public class LinkedListDemo {
+    public static void main(String[] args) {
+        Node node1=new Node(1);
+        Node node2=new Node(2);
+        Node node3=new Node(3);
+        Node node4=new Node(4);
+
+        node1.next=node2;
+        node2.next=node3;
+        node3.next=node4;
+
+        System.out.println(node1.id);
+        System.out.println(node1.next.id + " Id of the second node");
+
+    }
 }
