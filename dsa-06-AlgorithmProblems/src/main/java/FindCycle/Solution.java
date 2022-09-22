cf1657cba05bf8292b75e7e500498d552cf8f538
Fatih-Cyber <fatih@cybertekschool.com>
2022-09-22 21:23:38 +0300

    LinkedLists indexOf added

--- dsa-06-AlgorithmProblems/src/main/java/FindCycle/Solution.java
+++ dsa-06-AlgorithmProblems/src/main/java/FindCycle/Solution.java
@@ -12,11 +12,19 @@ public class Solution {
         n1.next=n2;
         n2.next=n3;
         n3.next=n4;
-        n4.next=null;
+        n4.next=n2;
         System.out.println(hasCycle(n1));
     }
     public static boolean hasCycle(ListNode head) {
-
+        // create a HashSet for nodes
+        HashSet<ListNode> visited=new HashSet<>();
+        // set current to head
+        ListNode current=head;
+        while(current!=null){
+            if (visited.contains(current)) return true;
+            visited.add(current);
+            current=current.next;
+        }
         return false;
     }
 }
