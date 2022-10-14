baf933dd8be829deab7f9de57e9fac28acef3a54
Fatih-Cyber <71564472+Fatih-Cyber@users.noreply.github.com>
2022-10-14 18:39:56 +0200

    Queues added

---
+++ dsa-07-Queues/src/main/java/CollectionsQueue.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.TreeMap;
+
+public class CollectionsQueue {
+    public static void main(String[] args) {
+
+    }
+}
